5ef1e5f1002738a02a4a9bc2ae834dcde2822214
Shobhit Nigam <50029224+shobhit-nigam@users.noreply.github.com>
2019-07-05 15:44:01 +0530

    Add files via upload

---
+++ 17june/day3/email2.ps1
@@ -0,0 +1,14 @@
+﻿$From = 'Trainer.Nigam@gmail.com'
+$To = 'deepak.hada@infineon.com'
+$attach = 'C:\Users\ushan\Desktop\infe\ping.txt'
+$sub = 'test mail twice'
+$body = 'powershell training going on'
+$server = 'smtp.gmail.com'
+$port = 587
+$secpsswd = ConvertTo-SecureString 'nigam.trainer' -AsPlainText -Force
+$username = 'Trainer.Nigam@gmail.com'
+
+$credobj = new-Object System.Management.Automation.PSCredential ($username, $secpsswd)
+
+Send-MailMessage -From $From -to $To -Subject $sub -Body $body -SmtpServer $server -port $port -UseSsl -Credential $credobj -Attachments $attach 
+#Send-MailMessage -
