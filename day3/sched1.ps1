a966880e12cd05df21bc6da836d4cf6bcbaa6b56
Shobhit Nigam <50029224+shobhit-nigam@users.noreply.github.com>
2019-07-05 11:44:41 +0530

    Add files via upload

---
+++ day3/sched1.ps1
@@ -0,0 +1,18 @@
+﻿$users = @()
+
+$sechtasks = Get-ScheduledTask 
+foreach ($Task in $sechtasks) {
+    $tempUser = $Task.Principal.UserId
+    switch ( $Task.Principal.UserId) {
+        "NETWORK SERVICE" { $continue = "skip this user"}
+        "LOCAL SERVICE" {$continue =  "skip this user"}
+        "SYSTEM" { $continue = "skip this user"}
+        "$null" {$continue =  "skip this user"}
+    default {$continue = "Report User"}
+    }
+    if ($continue -eq "Report User"){
+        $users = $users + $tempUser
+        }
+ }
+ $users.Count
+ $users | Get-Unique
---
+++ day3/sched2.ps1
@@ -0,0 +1,9 @@
+﻿$users = @()
+
+$sechtasks = Get-ScheduledTask 
+foreach ($Task in $sechtasks) {
+    $tempUser = $Task.Principal.UserId
+        $users = $users + $tempUser
+ }
+ $users.Count
+ $users | Get-Unique
