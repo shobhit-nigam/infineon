e3d47dda7cc8f2c35f0d78bb930144ae2f1c3efb
Shobhit Nigam <50029224+shobhit-nigam@users.noreply.github.com>
2019-07-05 11:46:32 +0530

    Delete sched2.ps1

--- day3/sched2.ps1
+++
@@ -1,9 +0,0 @@
-﻿$users = @()
-
-$sechtasks = Get-ScheduledTask 
-foreach ($Task in $sechtasks) {
-    $tempUser = $Task.Principal.UserId
-        $users = $users + $tempUser
- }
- $users.Count
- $users | Get-Unique
