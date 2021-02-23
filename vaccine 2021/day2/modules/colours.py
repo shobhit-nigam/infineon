9eed2ff156987d398a2c519fa57fab1300bbdddc
Shobhit Nigam <50029224+shobhit-nigam@users.noreply.github.com>
2021-02-24 02:59:37 +0530

    Add files via upload

---
+++ vaccine 2021/day2/modules/colours.py
@@ -0,0 +1,14 @@
+def blue():
+    print("cool blue")
+    
+def yellow():
+    print("bright yellow")
+    
+    
+def green():
+    print("mixture of ")
+    blue()
+    yellow()
+    
+def red():
+    print("in colours red")
